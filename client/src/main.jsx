d68abf46353989e08b99ec915f445e1614df01ac
MSITE.TOP <admin@msite.top>
2024-10-21 11:40:56 +0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './style.css';
 import './mobile.css';
+import 'https://d5d2sf46dn7jn3s1a6ei.apigw.yandexcloud.net/static/gpt.css';
+import 'https://d5d2sf46dn7jn3s1a6ei.apigw.yandexcloud.net/static/gpt.js';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
 
 const container = document.getElementById('root');
