f94acb4fd94a8d2685aade48722c2f020560cc3d
MSITE.TOP <admin@msite.top>
2024-10-22 09:15:03 +0400

    Update tx.js

--- api/models/tx.js
+++ api/models/tx.js
@@ -3,26 +3,26 @@ const defaultRate = 6;
 
 /** AWS Bedrock pricing */
 const bedrockValues = {
-  'llama2-13b': { prompt: 9.00, completion: 12.00 },
-  'llama2-70b': { prompt: 23.40, completion: 30.72 },
-  'llama3-8b': { prompt: 3.60, completion: 7.20 },
-  'llama3-70b': { prompt: 31.80, completion: 42.00 },
-  'llama3-1-8b': { prompt: 3.60, completion: 7.20 },
-  'llama3-1-70b': { prompt: 31.80, completion: 42.00 },
-  'llama3-1-405b': { prompt: 63.84, completion: 192.00 },
-  'mistral-7b': { prompt: 1.80, completion: 2.40 },
-  'mistral-small': { prompt: 1.80, completion: 2.40 },
-  'mixtral-8x7b': { prompt: 5.40, completion: 8.40 },
-  'mistral-large-2402': { prompt: 48.00, completion: 144.00 },
-  'mistral-large-2407': { prompt: 36.00, completion: 108.00 },
-  'command-text': { prompt: 18.00, completion: 24.00 },
-  'command-light': { prompt: 3.60, completion: 7.20 },
-  'ai21.j2-mid-v1': { prompt: 150.00, completion: 150.00 },
-  'ai21.j2-ultra-v1': { prompt: 225.60, completion: 225.60 },
-  'ai21.jamba-instruct-v1:0': { prompt: 6.00, completion: 8.40 },
-  'amazon.titan-text-lite-v1': { prompt: 1.80, completion: 2.40 },
-  'amazon.titan-text-express-v1': { prompt: 2.40, completion: 7.20 },
-  'amazon.titan-text-premier-v1:0': { prompt: 6.00, completion: 18.00 },
+  'llama2-13b': { prompt: 0.825, completion: 1.1 },
+  'llama2-70b': { prompt: 2.145, completion: 2.816 },
+  'llama3-8b': { prompt: 0.33, completion: 0.66 },
+  'llama3-70b': { prompt: 2.915, completion: 3.85 },
+  'llama3-1-8b': { prompt: 0.33, completion: 0.66 },
+  'llama3-1-70b': { prompt: 2.915, completion: 3.85 },
+  'llama3-1-405b': { prompt: 5.852, completion: 16.8 },
+  'mistral-7b': { prompt: 0.165, completion: 0.22 },
+  'mistral-small': { prompt: 0.165, completion: 0.22 },
+  'mixtral-8x7b': { prompt: 0.495, completion: 0.77 },
+  'mistral-large-2402': { prompt: 4.4, completion: 13.2 },
+  'mistral-large-2407': { prompt: 3.3, completion: 9.9 },
+  'command-text': { prompt: 1.65, completion: 2.2 },
+  'command-light': { prompt: 0.33, completion: 0.66 },
+  'ai21.j2-mid-v1': { prompt: 13.75, completion: 13.75 },
+  'ai21.j2-ultra-v1': { prompt: 20.68, completion: 20.68 },
+  'ai21.jamba-instruct-v1:0': { prompt: 0.55, completion: 0.77 },
+  'amazon.titan-text-lite-v1': { prompt: 0.165, completion: 0.22 },
+  'amazon.titan-text-express-v1': { prompt: 0.22, completion: 0.66 },
+  'amazon.titan-text-premier-v1:0': { prompt: 0.55, completion: 1.65 },
 };
 
 /**
@@ -32,35 +32,33 @@ const bedrockValues = {
  */
 const tokenValues = Object.assign(
   {
-    '8k': { prompt: 360, completion: 720 },
-    '32k': { prompt: 720, completion: 1440 },
-    '4k': { prompt: 18, completion: 24 },
-    '16k': { prompt: 36, completion: 48 },
-    'gpt-3.5-turbo-1106': { prompt: 12, completion: 24 },
-    'o1-preview': { prompt: 180, completion: 720 },
-    'o1-mini': { prompt: 36, completion: 144 },
-    o1: { prompt: 180, completion: 720 },
-    'gpt-4o-mini': { prompt: 1.80, completion: 7.20 },
-    'gpt-4o': { prompt: 30.00, completion: 120.00 },
-    'gpt-4o-2024-05-13': { prompt: 60.00, completion: 180.00 },
-    'gpt-4-1106': { prompt: 120.00, completion: 360.00 },
-    'gpt-3.5-turbo-0125': { prompt: 6.00, completion: 18.00 },
-    'claude-3-opus': { prompt: 180.00, completion: 900.00 },
-    'claude-3-sonnet': { prompt: 36.00, completion: 180.00 },
-    'claude-3-5-sonnet': { prompt: 36.00, completion: 180.00 },
-    'claude-3.5-sonnet': { prompt: 36.00, completion: 180.00 },
-    'claude-3-haiku': { prompt: 3.00, completion: 15.00 },
-    'claude-2.1': { prompt: 96.00, completion: 288.00 },
-    'claude-2': { prompt: 96.00, completion: 288.00 },
-    'claude-instant': { prompt: 9.60, completion: 28.80 },
-    'claude-': { prompt: 9.60, completion: 28.80 },
-    'command-r-plus': { prompt: 36.00, completion: 180.00 },
-    'command-r': { prompt: 6.00, completion: 18.00 },
-    /* cohere doesn't have rates for the older command models,
-  so this was from https://artificialanalysis.ai/models/command-light/providers */
-    command: { prompt: 4.56, completion: 4.56 },
-    'gemini-1.5': { prompt: 84.00, completion: 252.00 }, // May 2nd, 2024 pricing
-    gemini: { prompt: 6.00, completion: 18.00 }, // May 2nd, 2024 pricing
+    '8k': { prompt: 33, completion: 66 },
+    '32k': { prompt: 66, completion: 132 },
+    '4k': { prompt: 1.65, completion: 2.2 },
+    '16k': { prompt: 3.3, completion: 4.4 },
+    'gpt-3.5-turbo-1106': { prompt: 1.1, completion: 2.2 },
+    'o1-preview': { prompt: 16.5, completion: 66 },
+    'o1-mini': { prompt: 3.3, completion: 13.2 },
+    o1: { prompt: 16.5, completion: 66 },
+    'gpt-4o-mini': { prompt: 0.165, completion: 0.66 },
+    'gpt-4o': { prompt: 2.75, completion: 11 },
+    'gpt-4o-2024-05-13': { prompt: 5.5, completion: 16.5 },
+    'gpt-4-1106': { prompt: 11, completion: 33 },
+    'gpt-3.5-turbo-0125': { prompt: 0.55, completion: 1.65 },
+    'claude-3-opus': { prompt: 16.5, completion: 82.5 },
+    'claude-3-sonnet': { prompt: 3.3, completion: 16.5 },
+    'claude-3-5-sonnet': { prompt: 3.3, completion: 16.5 },
+    'claude-3.5-sonnet': { prompt: 3.3, completion: 16.5 },
+    'claude-3-haiku': { prompt: 0.275, completion: 1.375 },
+    'claude-2.1': { prompt: 8.8, completion: 26.4 },
+    'claude-2': { prompt: 8.8, completion: 26.4 },
+    'claude-instant': { prompt: 0.88, completion: 2.64 },
+    'claude-': { prompt: 0.88, completion: 2.64 },
+    'command-r-plus': { prompt: 3.3, completion: 16.5 },
+    'command-r': { prompt: 0.55, completion: 1.65 },
+    command: { prompt: 0.418, completion: 0.418 },
+    'gemini-1.5': { prompt: 7.7, completion: 23.1 }, // May 2nd, 2024 pricing
+    gemini: { prompt: 0.55, completion: 1.65 }, // May 2nd, 2024 pricing
   },
   bedrockValues,
 );
@@ -72,9 +70,9 @@ const tokenValues = Object.assign(
  * @type {Object.<string, {write: number, read: number }>}
  */
 const cacheTokenValues = {
-  'claude-3.5-sonnet': { write: 45.00, read: 3.60 },
-  'claude-3-5-sonnet': { write: 45.00, read: 3.60 },
-  'claude-3-haiku': { write: 3.60, read: 0.36 },
+  'claude-3.5-sonnet': { write: 4.125, read: 0.33 },
+  'claude-3-5-sonnet': { write: 4.125, read: 0.33 },
+  'claude-3-haiku': { write: 0.33, read: 0.033 },
 };
 
 /**
