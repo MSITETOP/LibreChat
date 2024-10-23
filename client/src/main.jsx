220d3a221e9b7063b80b199894de835ef919edfb
MSITE.TOP <admin@msite.top>
2024-10-23 19:53:38 +0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,29 @@ import App from './App';
 import './style.css';
 import './mobile.css';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
+import { useEffect } from 'react';
 
+export default function LoadCSS({ href }) {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = href;
+    document.head.appendChild(link);
+    
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, [href]);
+
+  return null; // Компонент ничего не рендерит, просто загружает CSS
+}
+const customCSS = process.env.CUSTOM_CSS;
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <ApiErrorBoundaryProvider>
+    <LoadCSS href={customCSS} />
     <App />
   </ApiErrorBoundaryProvider>,
 );
