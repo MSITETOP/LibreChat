b52bea5ba97f3d6ca2a98370f6ad45f7399ebef2
MSITE.TOP <admin@msite.top>
2024-10-23 18:58:46 +0400

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,6 @@ import App from './App';
 import './style.css';
 import './mobile.css';
 import { ApiErrorBoundaryProvider } from './hooks/ApiErrorBoundaryContext';
-if (process.env.CUSTOM_CSS) {
-import process.env.CUSTOM_CSS;
-}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
